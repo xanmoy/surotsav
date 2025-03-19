b3144c9cb1981dadc21492f1a38f560185ff138b
Tanmoy Ganguly <xanmoy.tsx@gmail.com>
2025-03-19 21:59:42 +0530

    updated

--- components/banner.tsx
+++ components/banner.tsx
@@ -35,21 +35,21 @@ export default function Banner({ onClose }: { onClose: () => void }) {
         <>
             <div className="fixed top-0 left-0 w-full z-[100]">
                 {showBanner1 && (
-                    <div className="banner w-full hover:text-blue-700 bg-gradient-to-r from-purple-600 to-pink-600 hover:from-purple-700 hover:to-pink-700 text-white py-2 px-4 flex items-center justify-between sm:justify-start lg:justify-center">
+                    <div className="banner w-full hover:text-gray-300 bg-gradient-to-r from-purple-600 to-pink-600 text-white py-2 px-4 flex items-center justify-between sm:justify-start lg:justify-center">
                         <a href="https://drive.google.com/file/d/1fuq1pp8DivWegrMAiExqszpSdTclNnA7/view?usp=sharing">
                             <span className="text-sm md:text-base text-left lg:text-center">🚀 Manthan 2025, the annual tech fest, is set for March 19-20, 2025! 🎉</span>
                         </a>
-                        <Button variant="ghost" className="text-white lg:absolute lg:right-4" onClick={() => setShowBanner1(false)}>
+                        <Button className="text-white bg-transparent hover:bg-pink-700 lg:absolute lg:right-4" onClick={() => setShowBanner1(false)}>
                             <X className="w-5 h-5" />
                         </Button>
                     </div>
                 )}
                 {showBanner2 && (
-                    <div className="banner w-full hover:text-blue-900 bg-blue-600 text-white py-2 px-4 flex items-center justify-between sm:justify-start lg:justify-center">
+                    <div className="banner w-full hover:text-gray-300 bg-blue-600 text-white py-2 px-4 flex items-center justify-between sm:justify-start lg:justify-center">
                         <a href="https://drive.google.com/file/d/1FytlZfxRJH_dfnOlxSlK62S8185JJdR4/view?usp=sharing">
                             <span className="text-sm md:text-base text-left lg:text-center">📢 Important Notice: 'Udaan' and 'Tarang' fests are postponed to April 2nd & 3rd due to heavy rainfall. 🎟️</span>
                         </a>
-                        <Button variant="ghost" className="text-white lg:absolute lg:right-4" onClick={() => setShowBanner2(false)}>
+                        <Button className="text-white bg-transparent hover:bg-blue-700 lg:absolute lg:right-4" onClick={() => setShowBanner2(false)}>
                             <X className="w-5 h-5" />
                         </Button>
                     </div>
